7cdb9570413bf4510eace4de45e29ae5a53900ea
zohaib <zohaibansari100@gmail.com>
2021-06-07 12:30:38 +0530

    Initial commit

---
+++ ElementHistoryDialog/src/main/java/me/zed/elementhistorydialog/ElementHistoryDialog.java
@@ -0,0 +1,11 @@
+package me.zed.elementhistorydialog;
+
+public class ElementHistoryDialog {
+
+    /**
+    * method to show the dialog, that takes the osm id, type as an input
+    */
+    public void show(){
+
+    }
+}
