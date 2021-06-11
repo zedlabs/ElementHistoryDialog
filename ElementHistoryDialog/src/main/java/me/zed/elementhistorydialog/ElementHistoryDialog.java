2628ca529724bf0b0eae102c1e2a4ff9b5678d69
zohaib <zohaibansari100@gmail.com>
2021-06-11 10:08:03 +0530

    fix Dialog creation

--- ElementHistoryDialog/src/main/java/me/zed/elementhistorydialog/ElementHistoryDialog.java
+++ ElementHistoryDialog/src/main/java/me/zed/elementhistorydialog/ElementHistoryDialog.java
@@ -12,6 +12,25 @@ import androidx.fragment.app.DialogFragment;
 
 public class ElementHistoryDialog extends DialogFragment {
 
+    private long osmId;
+    private String elementType;
+
+    /**
+     * Method that will create a new instance of the Dialog
+     *
+     * @param osmId       the id of the OSM element to be displayed
+     * @param elementType the OSM element type
+     * @return instance of the Dialog
+     */
+    public static ElementHistoryDialog create(long osmId, String elementType) {
+        return new ElementHistoryDialog(osmId, elementType);
+    }
+
+    private ElementHistoryDialog(long osmId, String elementType) {
+        this.osmId = osmId;
+        this.elementType = elementType;
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -21,6 +40,7 @@ public class ElementHistoryDialog extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.edit_selection_screen, null);
     }
 }
