64869158f001315a0a98ce3077f151791beed9a2
zohaib <zohaibansari100@gmail.com>
2021-06-11 09:27:29 +0530

    make initial screen dialog fragment

--- ElementHistoryDialog/src/main/java/me/zed/elementhistorydialog/ElementHistoryDialog.java
+++ ElementHistoryDialog/src/main/java/me/zed/elementhistorydialog/ElementHistoryDialog.java
@@ -1,11 +1,26 @@
 package me.zed.elementhistorydialog;
 
-public class ElementHistoryDialog {
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-    /**
-    * method to show the dialog, that takes the osm id, type as an input
-    */
-    public void show(){
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
+public class ElementHistoryDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.edit_selection_screen, null);
     }
 }
